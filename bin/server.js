5d334e6997c8371441b8112fa24cba312aa984cc
moonzera <introducaoteste@gmail.com>
2024-11-11 02:04:42 -0300

    Adicionando mensagem quando o servidor é ligado

--- bin/server.js
+++ bin/server.js
@@ -1,54 +1,56 @@
-var app = require('../app');
+var app = require("../app")
 
-var debug = require('debug')('node-rest:server');
+var debug = require("debug")("node-rest:server")
 
-const http = require("http");
+const http = require("http")
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+const normalizePort = (val) => {
+  var port = parseInt(val, 10)
 
   if (isNaN(port)) {
-    // named pipe 
-    return val;
+    // named pipe
+    return val
   }
 
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
 
-  return false;
-};
+  return false
+}
 
-const onError = error => {
+const onError = (error) => {
   if (error.syscall !== "listen") {
-    throw error;
+    throw error
   }
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port
   switch (error.code) {
     case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
+      console.error(bind + " requires elevated privileges")
+      process.exit(1)
+      break
     case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
+      console.error(bind + " is already in use")
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
-};
+}
 
 const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
-};
+  const addr = server.address()
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port
+  debug("Listening on " + bind)
+}
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || "3000")
+app.set("port", port)
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+const server = http.createServer(app)
+server.on("error", onError)
+server.on("listening", onListening)
+server.listen(port, "localhost", () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
